e542149684cb5abb4a270d21f2ae86f09cffbaac
benjaminshellwe <benjaminshellwe@outlook.com>
2022-12-05 17:56:50 +0800

    version 1.b.4.3

--- src/test/java/top/shellwe/logreadermod/controller/LinuxControllerTest.java
+++ src/test/java/top/shellwe/logreadermod/controller/LinuxControllerTest.java
@@ -1,13 +1,14 @@
 package top.shellwe.logreadermod.controller;
 
 import org.junit.jupiter.api.Test;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinuxControllerTest {
-
+    private static final Logger logger = LogManager.getLogger("HelloWorld");
     @Test
     void getConnect() {
-
+        logger.info("Hello, shellwe is trying coding logging！");
     }
 }
