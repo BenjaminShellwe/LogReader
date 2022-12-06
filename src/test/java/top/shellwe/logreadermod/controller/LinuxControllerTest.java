5d8bff6a14d39e425d93d809009ab6eccb6b12a7
benjaminshellwe <benjaminshellwe@outlook.com>
2022-12-06 17:03:33 +0800

    version 1.b.4.t1

--- src/test/java/top/shellwe/logreadermod/controller/LinuxControllerTest.java
+++ src/test/java/top/shellwe/logreadermod/controller/LinuxControllerTest.java
@@ -6,9 +6,17 @@ import org.apache.logging.log4j.Logger;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinuxControllerTest {
-    private static final Logger logger = LogManager.getLogger("HelloWorld");
+    private static final Logger logger = LogManager.getLogger("logger test");
     @Test
     void getConnect() {
         logger.info("Hello, shellwe is trying coding logging");
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Logging debug");
+        }
+
+        logger.debug("Integer.MAX_VALUE = %,d", Integer.MAX_VALUE);
+        logger.debug("Long.MAX_VALUE = %,d", Long.MAX_VALUE);
+        logger.error("Unable to process request due to {}");
     }
 }
