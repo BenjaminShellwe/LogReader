705f2b2f2a3c5f628f74b696c223b99b5c9c4ba8
shellwe <benjaminshellwe@outlook.com>
2022-12-11 22:21:21 +0800

    version 1.b.6.9

--- src/test/java/top/shellwe/logreadermod/controller/LinuxControllerTest.java
+++ src/test/java/top/shellwe/logreadermod/controller/LinuxControllerTest.java
@@ -1,15 +1,10 @@
 package top.shellwe.logreadermod.controller;
 
 import org.junit.jupiter.api.Test;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class LinuxControllerTest {
 
